f08cc23230388d5a8ac87523459b287ec84e5f8c
RedSova777 <DEN0112K@yandex.ru>
2026-06-13 14:11:37 +0300

    Тренировка добавления 1

---
+++ crypto_1.py
@@ -0,0 +1,59 @@
+# -*- coding: cp1251 -*-
+#!/usr/bin/env python3
+
+"""
+Средство извлечения параметров RSA из файла закрытого ключа OpenSSH.
+
+Поддерживаемые входные данные:
+    -----BEGIN OPENSSH PRIVATE KEY-----
+              ...base64...
+    -----END OPENSSH PRIVATE KEY-----
+
+Выходные данные:
+    файл json
+
+"""
+
+import argparse
+import base64
+import binascii
+import json
+import os
+import sys
+from typing import Optional
+
+
+def parser_arg() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Извлечение n, e, d из ключа OpenSSH RSA private key"
+    )
+    parser.add_argument("input", help="путь к файлу id_rsa в стандартном формате OpenSSH private key")
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="путь к выходному файлу; если выходной файл не указан, результат выводится в stdout",
+    )
+    parser.add_argument(
+        "--format",
+        choices=("text", "json"),
+        default="text",
+        help="формат вывода данных: text или json; по умолчанию text",
+    )
+    return parser
+
+
+def main(argv: Optional[list[str]] = None) -> int:
+    parser = parser_arg()
+    args = parser.parse_args(argv)
+    print(args)
+    
+    try:
+        #binary = load_pem_openssh_private_key(args.input)
+        #params = parse_openssh_rsa_private_key(binary)
+        #write_output(params, args.output, args.format)
+        return 0
+    except KeyboardInterrupt:
+        print("Ошибка: выполнение прервано пользователем", file=sys.stderr)
+        return 2
+
+main()
